65739c61834dc970aaf8e635f8f6ce856bc275f4
DELL <DELL@SIDHU>
2023-08-29 21:59:37 +0530

    first commit

---
+++ src/collections/ArrayListExp.java
@@ -0,0 +1,15 @@
+package collections;
+import java.util.*;
+
+public class ArrayListExp {
+public static void main(String[] args) {
+	ArrayList al= new ArrayList();
+	al.add(33);
+	al.add("siddarth");
+	al.add(94);
+	al.add(90);
+	al.add('s');
+	al.add(0.3f);
+	System.out.println(al);
+}
+}
---
+++ src/collections/LinkedListExp.java
@@ -0,0 +1,19 @@
+package collections;
+
+
+import java.util.*;
+
+public class LinkedListExp {
+public static void main(String[] args) {
+	LinkedList l= new LinkedList();
+	l.add(90);
+	l.add(1);
+	l.add("sid");
+	l.add(90);
+	l.add(1);
+	l.add("sid");
+	System.out.println(l);
+	
+	
+}
+}
